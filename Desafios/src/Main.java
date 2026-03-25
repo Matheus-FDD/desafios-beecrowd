7bbdd53605a2758fd9ed365911d824f62402d550
matheus <matheuslopes201805@gmail.com>
2026-03-25 14:17:50 -0300

    desafio 1156

--- Desafios/src/Main.java
+++ Desafios/src/Main.java
@@ -3,12 +3,16 @@ public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        double s,n;
-        n=0;
-        for (double i =1;i<=100;i++){
-            s=1/i;
-            n=n+s;
-        }System.out.printf("%.2f%n", n);
+        double s,n,soma;
+        n=1;
+        soma = 0;
+
+        for (double i =1;i<=39;i=i+2){
+            s=i/n;
+            n=n*2;
+            soma = soma+s;
+
+        }System.out.printf("%.2f%n",soma);
 
 
         sc.close();
